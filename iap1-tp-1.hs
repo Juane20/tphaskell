50f895fe4853921d2a0fb69f09388840e9fa426f
tu gatita <compurandom@labo.com>
2023-05-04 15:06:57 -0300

    Varios predicados estaban mal hechos e hice el ejercicio 1.

--- iap1-tp-1.hs
+++ iap1-tp-1.hs
@@ -6,6 +6,51 @@
 -- Integrante 3: Nombre Apellido, email, LU
 -- Integrante 4: Nombre Apellido, email, LU
 
+-- Ejemplos
+
+usuario1 = (1, "Juan")
+usuario2 = (2, "Natalia")
+usuario3 = (3, "Pedro")
+usuario4 = (4, "Mariela")
+usuario5 = (5, "Natalia")
+
+relacion1_2 = (usuario1, usuario2)
+relacion1_3 = (usuario1, usuario3)
+relacion1_4 = (usuario4, usuario1) -- Notar que el orden en el que aparecen los usuarios es indistinto
+relacion2_3 = (usuario3, usuario2)
+relacion2_4 = (usuario2, usuario4)
+relacion3_4 = (usuario4, usuario3)
+
+publicacion1_1 = (usuario1, "Este es mi primer post", [usuario2, usuario4])
+publicacion1_2 = (usuario1, "Este es mi segundo post", [usuario4])
+publicacion1_3 = (usuario1, "Este es mi tercer post", [usuario2, usuario5])
+publicacion1_4 = (usuario1, "Este es mi cuarto post", [])
+publicacion1_5 = (usuario1, "Este es como mi quinto post", [usuario5])
+
+publicacion2_1 = (usuario2, "Hello World", [usuario4])
+publicacion2_2 = (usuario2, "Good Bye World", [usuario1, usuario4])
+
+publicacion3_1 = (usuario3, "Lorem Ipsum", [])
+publicacion3_2 = (usuario3, "dolor sit amet", [usuario2])
+publicacion3_3 = (usuario3, "consectetur adipiscing elit", [usuario2, usuario5])
+
+publicacion4_1 = (usuario4, "I am Alice. Not", [usuario1, usuario2])
+publicacion4_2 = (usuario4, "I am Bob", [])
+publicacion4_3 = (usuario4, "Just kidding, i am Mariela", [usuario1, usuario3])
+
+
+usuariosA = [usuario1, usuario2, usuario3, usuario4]
+relacionesA = [relacion1_2, relacion1_4, relacion2_3, relacion2_4, relacion3_4]
+publicacionesA = [publicacion1_1, publicacion1_2, publicacion2_1, publicacion2_2, publicacion3_1, publicacion3_2, publicacion4_1, publicacion4_2]
+redA = (usuariosA, relacionesA, publicacionesA)
+
+usuariosB = [usuario1, usuario2, usuario3, usuario5]
+relacionesB = [relacion1_2, relacion2_3]
+publicacionesB = [publicacion1_3, publicacion1_4, publicacion1_5, publicacion3_1, publicacion3_2, publicacion3_3]
+redB = (usuariosB, relacionesB, publicacionesB)
+
+
+
 type Usuario = (Integer, String) -- (id, nombre) Ej (1, "Kevin")     [1,2] [2,1]
 type Relacion = (Usuario, Usuario) -- usuarios que se relacionan Ej ((6, "Kevin"), (2, "Angel"), ((2, "Angel"), (6, "Kevin")) Esto solo contiene UN ELEMENTO
 type Publicacion = (Usuario, String, [Usuario]) -- (usuario que publica, texto publicacion, likes)
@@ -38,43 +83,59 @@ likesDePublicacion (_, _, us) = us
 
 -- Predicados/Funciones auxiliares
 
-pertenece :: (Eq t) => t -> [t] -> Bool
+pertenece :: (Eq t) => t -> [t] -> Bool -- Pertenece el elemento de tipo t a la lista de elementos de tipo t
 pertenece _ [] = False
 pertenece a (x:xs) | a == x = True
                    | otherwise = pertenece a xs
 
-empiezaCon :: (Eq t) => [t] -> t
-empiezaCon [] = error "La lista se encuentra vacía"
-empiezaCon (x:xs) = x
+empiezaCon :: (Eq t) => t -> [t] -> Bool -- Comprueba si el elemento de tipo t es el primer elemento de la lista de tipo t.
+empiezaCon _ [] = False
+empiezaCon x (y:ys) = x == y
 
-terminaCon :: (Eq t) => [t] -> t
-terminaCon [] = error "La lista se encuentra vacía"
-terminaCon [x] = x
-terminaCon (x:xs) = terminaCon xs
+ultimoElemento :: (Eq t) => [t] -> t -- Dada una lista, devuelve el último elemento.
+ultimoElemento [] = undefined
+ultimoElemento [x] = x
+ultimoElemento (x:xs) = ultimoElemento xs
 
-sinRepetidos :: (Eq t) => [t] -> Bool
+terminaCon :: (Eq t) => t -> [t] -> Bool -- Comprueba si el elemento de tipo t es el último elemento de la lista de tipo t.
+terminaCon _ [] = False
+terminaCon x (y:ys) = ultimoElemento(y:ys) == x
+
+sinRepetidos :: (Eq t) => [t] -> Bool -- Comprueba que una lista no tenga repetidos
 sinRepetidos [] = True
 sinRepetidos (x:xs) | pertenece x xs = False
                     | otherwise = sinRepetidos xs
 
---PREGUNTAR EN CLASE EL CASO [1,2,3] [1,2,3,3]
-mismosElementos :: (Eq t) => [t] -> [t] -> Bool
+quitar :: (Eq t) => t -> [t] -> [t] -- Devuelve la lista quitando una vez el elemento.
+quitar _ [] = undefined
+quitar x (y:ys)
+    | x /= y = y:quitar x ys
+    | otherwise = ys
+
+mismosElementos :: (Eq t) => [t] -> [t] -> Bool -- Comprueba que dos listas tengan los mismos elementos (sin repetidos)
 mismosElementos [] [] = True
 mismosElementos _ [] = False
-mismosElementos [] _ = False 
-mismosElementos (x:xs) (y:ys) | pertenece x (y:ys) = mismosElementos (x:xs) ys
+mismosElementos [] _ = False amigosDe x y
+    | if pertenece y
+mismosElementos (x:xs) (y:ys) | pertenece x (y:ys) = mismosElementos xs (quitar x (y:ys))
                               | otherwise = False
                               
-usuarioValido :: Usuario -> Bool --(1, "Kevin")
+usuarioValido :: Usuario -> Bool --(1, "Kevin") -- Comprueba que un usuario sea válido.
 usuarioValido a | idDeUsuario a > 0 && nombreDeUsuario a /= "" = True
                 | otherwise = False
-noHayIdsRepetidos :: [Usuario] -> Bool
-noHayIdsRepetidos [] = True
-noHayIdsRepetidos (x:xs) = not (pertenece x xs) && noHayIdsRepetidos xs
 
-usuariosValidos :: [Usuario] -> Bool --[(1, "Kevin"), (2, "Angel")etc...]
+concatenarIdDeUsuarios :: [Usuario] -> [Integer] -- Concateno en una lista los ID de los usuarios.
+concatenarIdDeUsuarios [] = []
+concatenarIdDeUsuarios (x:xs) = (idDeUsuario(x):concatenarIdDeUsuarios(xs))
+
+noHayIdsRepetidos :: [Usuario] -> Bool -- Comprueba que, dada una lista de usuarios, no haya id's repetidos.
+noHayIdsRepetidos [] = True
+noHayIdsRepetidos x = sinRepetidos(concatenarIdDeUsuarios x)
+
+usuariosValidos :: [Usuario] -> Bool --[(1, "Kevin"), (2, "Angel")etc...] -- Comprueba que, dada una lista de usuarios,todos los usuarios sean válidos.
 usuariosValidos [] = False
-usuariosValidos (x:xs) | usuarioValido x && noHayIdsRepetidos (x:xs) = True
+usuariosValidos [x] = usuarioValido x
+usuariosValidos (x:xs) | usuarioValido x && noHayIdsRepetidos (x:xs) = usuariosValidos xs
                        | otherwise = False
 
 usuariosDeRelacionValidos :: [Usuario] -> [Relacion] -> Bool--(a,b) a = 0 = (1, "Kevin"), b = 1 = (2, "Angel")
@@ -103,14 +164,18 @@ relacionadoDirecto a b (cs, d:ds, fs) | pertenece (a,b) (d:ds) || pertenece (b,a
                                      | otherwise = relacionadoDirecto a b (cs, ds, fs)
 -- Ejercicios 
 
-nombresDeUsuarios :: RedSocial -> [String]
-nombresDeUsuarios = undefined
+proyectarNombres :: [Usuario] -> [String] -- Dada una lista de usuarios, devuelvo los nombres.
+proyectarNombres [] = []
+proyectarNombres (x:xs) = (nombreDeUsuario x:proyectarNombres xs)
 
--- describir qué hace la función: .....
+nombresDeUsuarios :: RedSocial -> [String] -- Dada una red social, devuelvo los nombres de los usuarios.
+nombresDeUsuarios x = proyectarNombres(usuarios(x))
+
+-- describir qué hace la función: Dada una red social y un usuario, devuelve una lista de los amigos de ese usuario.
 amigosDe :: RedSocial -> Usuario -> [Usuario]
 amigosDe = undefined
 
--- describir qué hace la función: .....
+
 cantidadDeAmigos :: RedSocial -> Usuario -> Int
 cantidadDeAmigos = undefined
 
